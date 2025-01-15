6304be4be99bc1d9dcfffe412a2a4a8e9f84bf2d
Ludovico Verducci <ludovico.verducci@gmail.com>
2025-01-14 23:06:02 -0800

    Fixed basic build for visionOS. Chart can be visualized but scrolling does not work correctly

--- Source/Charts/Utils/Platform+Accessibility.swift
+++ Source/Charts/Utils/Platform+Accessibility.swift
@@ -11,7 +11,7 @@
 
 import Foundation
 
-#if os(iOS) || os(tvOS)
+#if os(iOS) || os(tvOS) || os(visionOS)
 import UIKit
 
 internal func accessibilityPostLayoutChangedNotification(withElement element: Any? = nil)
@@ -62,7 +62,11 @@ open class NSUIAccessibilityElement: UIAccessibilityElement
         set
         {
             guard let containerView = containerView else { return }
+#if !os(visionOS)
             super.accessibilityFrame = containerView.convert(newValue, to: UIScreen.main.coordinateSpace)
+#else
+            // FIXME
+#endif
         }
     }
 }
--- Source/Charts/Utils/Platform.swift
+++ Source/Charts/Utils/Platform.swift
@@ -3,7 +3,7 @@ import Foundation
 /** This file provides a thin abstraction layer atop of UIKit (iOS, tvOS) and Cocoa (OS X). The two APIs are very much 
 alike, and for the chart library's usage of the APIs it is often sufficient to typealias one to the other. The NSUI*
 types are aliased to either their UI* implementation (on iOS) or their NS* implementation (on OS X). */
-#if os(iOS) || os(tvOS)
+#if os(iOS) || os(tvOS) || os(visionOS)
 	import UIKit
 
 
@@ -13,7 +13,10 @@ types are aliased to either their UI* implementation (on iOS) or their NS* imple
 	public typealias NSUIFont = UIFont
 	public typealias NSUIImage = UIImage
 	public typealias NSUIScrollView = UIScrollView
+
+#if !os(visionOS)
     public typealias NSUIScreen = UIScreen
+#endif
 	public typealias NSUIDisplayLink = CADisplayLink
 
     extension NSUIColor
@@ -49,6 +52,7 @@ types are aliased to either their UI* implementation (on iOS) or their NS* imple
         }
     }
     
+#if !os(visionOS)
     extension UIScreen
     {
         @objc final var nsuiScale: CGFloat
@@ -61,6 +65,8 @@ types are aliased to either their UI* implementation (on iOS) or their NS* imple
     {
         return NSUIScreen.main
     }
+#endif
+    
 
 #endif
 
